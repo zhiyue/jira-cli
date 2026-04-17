93a70af1226918690abc20ed86d17c341f3e9c0a
zhiyue <jabari.dai@gmail.com>
2026-04-17 16:40:08 +0800

    chore: scaffold cargo project with module placeholders

---
+++ src/error.rs
@@ -0,0 +1,10 @@
+// placeholder
+pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+pub struct Error;
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error")
+    }
+}
+impl std::error::Error for Error {}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    eprintln!("jira-cli: scaffolded but not yet implemented");
+    std::process::exit(1);
+}
